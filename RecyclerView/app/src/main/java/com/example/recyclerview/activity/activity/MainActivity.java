d9176982af44a53e430e98de47bb29435fae9563
nathan-oliveira <nathan_oliveiramendonca@hotmail.com>
2020-08-11 10:50:19 -0300

    chore: add Project ReyclerView

--- RecyclerView/app/src/main/java/com/example/recyclerview/activity/activity/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/activity/activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.recyclerview.activity.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.recyclerview.R;
+import com.example.recyclerview.activity.adapter.Adapter;
+import com.example.recyclerview.activity.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +27,50 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
-        // Configurar adapter
+        // Listagem de filmes
+        this.criarFilmes();
 
+        // Configurar adapter
+        Adapter adapter = new Adapter(listaFilmes);
 
         // Configurar Recyclerview
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-//        recyclerView.setAdapter();
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+        recyclerView.setAdapter(adapter);
 
+    }
+
+    public void criarFilmes() {
+        Filme filme = new Filme("titulo", "genero", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo1", "genero1", "2018");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo2", "genero2", "2019");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo3", "genero3", "2020");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo4", "genero4", "2020");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo5", "genero5", "2020");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo6", "genero6", "2020");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo7", "genero7", "2020");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo8", "genero8", "2020");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("titulo9", "genero9", "2020");
+        this.listaFilmes.add(filme);
     }
 }
